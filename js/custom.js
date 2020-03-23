75fb062a7c084d3f503031598708be4d5aa187f0
Michael Mulhall <michaelmulhall@gmail.com>
2020-03-23 04:53:32 -0400

    img

--- js/custom.js
+++ js/custom.js
@@ -507,10 +507,3 @@ function playPauseRotating() {
  $(document).ready(function() {
         $('#rotate').rotaterator({fadeSpeed:500, pauseSpeed:5000});
  });
-
-
- $(function() {
-	setTimeout(function() {
-		$('.fly-in-text').removeClass('hidden');
-	},500);
-})();
